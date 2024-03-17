8af05903cc081e8db2740df2af120083a6e4c2c2
ananduaji <ananduaji0077@gmail.com>
2024-03-17 22:58:59 +0530

    removed unwated codes

--- Main/views.py
+++ Main/views.py
@@ -241,75 +241,11 @@ def get_answers(path, answers, negative_marking, questions, choices):
 
                 # The final Image
                 finalImg = cv2.addWeighted(finalImg, 1, imgInvGradeWarp, 1, 0)
-                # cv2.imshow('final image', finalImg)
 
                 return finalImg, score
-
-            imgBlank = np.zeros_like(img)
-            # making image array
-            imageArray = ([img, imgGray, imgBlur, imgCanny],
-                        [imgContours, imgBiggestContours, imgWarpColored, imgThresh],
-                        [imgResult, imgRawDrawing, imgInvWrap, finalImg])
             
         except:
-            imgBlank = np.zeros_like(img)
-            # making image array
-            imageArray = ([img, imgGray, imgBlur, imgCanny],
-                        [imgBlank, imgBlank, imgBlank, imgBlank],
-                        [imgBlank, imgBlank, imgBlank, imgBlank])
-
-        labels = [["Original","Gray","Blur","Canny"],
-                    ["Contours","Biggest con","Warpped","Threshold"],
-                    ["Result","Raw drawing","Inv Warpped","Final"]]
-
-        # imported function from utils to show the images used as a stack
-        imgStacked = stackImages(imageArray, 0.3, labels)
-
-        # cv2.imshow('Stacked images', imgStacked)
-        # cv2.imshow('final image', finalImg)
-        
-        key = cv2.waitKey(1) & 0xFF
-
-        # press s key to save the image
-        if key != 0xFF:
-            break
-
-    cv2.destroyAllWindows()
-
-def stackImages(imgArray,scale,lables=[]):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-            hor_con[x] = np.concatenate(imgArray[x])
-        ver = np.vstack(hor)
-        ver_con = np.concatenate(hor)
-    else:
-        for x in range(0, rows):
-            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        hor_con= np.concatenate(imgArray)
-        ver = hor
-    if len(lables) != 0:
-        eachImgWidth= int(ver.shape[1] / cols)
-        eachImgHeight = int(ver.shape[0] / rows)
-        for d in range(0, rows):
-            for c in range (0,cols):
-                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
-                cv2.putText(ver,lables[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
-    return ver
+            print('An Error Occured')
 
 # function to find out the rectangle edges
 def rectContour(contours):
